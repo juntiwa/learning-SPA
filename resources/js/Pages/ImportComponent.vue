21fda139fe1c1e3db8efc4e707b66adf42a4db04
juntiwa <juntima.nuch@gmail.com>
2023-01-02 14:28:21 +0700

    install setup inertia vue and tailwind css

---
+++ resources/js/Pages/ImportComponent.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-red-500">
+        Component
+    </div>
+
+</template>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    Test
+    <div class="bg-teal-500">
+        Text CSS
+    </div>
+    <ImportComponent/>
+</template>
+<script setup>
+
+import ImportComponent from "./ImportComponent";
+</script>
